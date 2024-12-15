1c10da1d8236614e36d054f72978687c4decd4b5
psierra7 <psierra100@gmail.com>
2024-12-15 10:18:45 -0500

    se define, compila y comienza el entrenamiento de la neurona

--- main.py
+++ main.py
@@ -32,3 +32,39 @@ df['tiempo_normalizado'] = scaler.fit_transform(df[['tiempo_viaje']])
 # Definir las carateristica
 X = df[['origen_encoded', 'destino_encoded']].values
 y = df['tiempo_normalizado'].values
+
+
+
+
+
+#-------------------------------------------Terminamos entrenamiento-------------------------------------------------------------------------------------------------------------------------------#
+
+
+
+
+#----------------------------------------------------------------------empezamos a definir, compilar el modelo y comenzar el entrenamiento-----------------------------------------------------------------------------------------------------------#
+
+# Dividir los datos en conjunto de entrenamiento y prueba
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# Construimos la neurona
+
+model = Sequential([
+    Embedding(input_dim=len(le.classes_), output_dim=4, input_length=2),
+    Flatten(),
+    Dense(16, activation='relu'),
+    Dense(1, activation='linear')
+])
+
+# Compilar el modelo
+model.compile(optimizer='adam', loss='mean_squared_error', metrics=['mae'])
+
+# Entrenamos el modelo
+history = model.fit(X_train, y_train, epochs=50, batch_size=4, validation_data=(X_test, y_test), verbose=1)
+
+# Evaluar el modelo
+loss, mae = model.evaluate(X_test, y_test, verbose=0)
+print(f"Mean Absolute Error en el conjunto de prueba: {mae:.4f}")
+
+#----------------------------------------------------------------------------------finalizacion de la compilacion e entrenamiento-----------------------------------------------------------------------------------------------#
+
+
